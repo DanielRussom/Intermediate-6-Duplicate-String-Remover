58b16bfd1c8a09503ad160e04a83260e8ebb8432
Daniel Russom <daniel.russom00@gmail.com>
2018-02-08 14:37:17 +0000

    Tidying and Commenting
    -Tidied and commented the code
    -Changed how certain functions work to allow for easier changes
    -Added function to display message
    -Added function to convert stored words to sentence
    -Original and updated messages are now displayed

--- src/duplicatestringremover/Controller.java
+++ src/duplicatestringremover/Controller.java
@@ -1,42 +1,50 @@
 package duplicatestringremover;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Controller {
 
 	public Controller() throws IOException {
+		// Sets up a stored file location
 		FileReader fileReader = new FileReader("bin/TestFile.txt");
-
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
 		ArrayList<String> lines = new ArrayList<String>();
 		String line = null;
-
+		// Reads in the file one line at a time
 		while ((line = bufferedReader.readLine()) != null) {
 			lines.add(line);
 		}
-
 		bufferedReader.close();
+
+		System.out.println("Original Message");
+		System.out.println("----------------");
+		// Calls a method to display the stored message
+		displayText(lines);
 		// Iterates through each string in lines
-		for (String current : lines) {
+		for (int i = 0; i < lines.size(); i++) {
 			// Splits each word up
-			String[] words = current.split("\\s+");
+			String[] words = lines.get(i).split("\\s+");
 			// Iterates through each word in the current line
-			for (String currentWord : words) {
+			for (int j = 0; j < words.length; j++) {
 				// Skips words with less than 3 characters
-				if (currentWord.length() < 3) {
+				if (words[j].length() < 3) {
 					continue;
 				}
 				// Calls a function to remove duplicate characters
-				currentWord = removeDuplicateChars(currentWord);
-
-				System.out.println(currentWord);
+				words[j] = removeDuplicateChars(words[j]);
 			}
+			// Replaces the current line with the new version
+			lines.set(i, convertToString(words));
 		}
+
+		System.out.println();
+		System.out.println("Updated Message");
+		System.out.println("---------------");
+		// Calls a method to display the new message
+		displayText(lines);
 	}
 
 	/**
@@ -47,30 +55,57 @@ public class Controller {
 	 * @return - passed in string with duplicate characters removed
 	 */
 	public String removeDuplicateChars(String word) {
-		String newWord = null;
 		// Iterates through each character in the current word, except for the last one
-		for (int i = 0; i < word.length() - 1; i++) {
-			char currentChar = Character.toLowerCase(word.charAt(i));
-			String temp = word.substring(i + 1).toLowerCase();
+		for (int i = 1; i < word.length(); i++) {
+			// Stores the current character to inspect
+			char currentChar = Character.toLowerCase(word.charAt(i - 1));
+			String temp = word.substring(i).toLowerCase();
 			int location = temp.indexOf(currentChar);
+			// Loops while the current character has a duplicate
 			while (location >= 0) {
-
-			//System.out.println(word.substring(0, location+i + 1) + " " + word.substring(location + i + 2));
-				word = word.substring(0, location+i + 1) + word.substring(location + i + 2);
-				//System.out.println(word);
-
-				temp = word.substring(i + 1).toLowerCase();
+				// Removes the duplicate character
+				word = word.substring(0, location + i) + word.substring(location + i + 1);
+				// Checks for another duplicate
+				temp = word.substring(i).toLowerCase();
 				location = temp.indexOf(currentChar);
 			}
 		}
 		return word;
 	}
 
+	/**
+	 * Displays the message in the passed in array list
+	 * 
+	 * @param message
+	 *            - message to display
+	 */
+	public void displayText(ArrayList<String> message) {
+		for (String current : message) {
+			System.out.println(current);
+		}
+	}
+
+	/**
+	 * Converts array of words to a sentence with spaces
+	 * 
+	 * @param words
+	 *            - array of words to be converted
+	 * @return - generated sentence
+	 */
+	public String convertToString(String[] words) {
+		String sentence = words[0];
+		// Appends each word onto the end of the current sentence
+		for (int i = 1; i < words.length; i++) {
+			sentence = sentence + " " + words[i];
+		}
+		return sentence;
+
+	}
+
 	public static void main(String[] args) {
 		try {
 			new Controller();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
